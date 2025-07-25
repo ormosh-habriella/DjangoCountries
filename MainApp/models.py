c2eb1f2eae0b86ecb46376ada194e2fe94cd2198
ormosh <ormoshg@gmail.com>
2025-07-25 23:15:43 +0200

    add model Country

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    languages = models.JSONField()
+
+    def __str__(self):
+        return self.name
